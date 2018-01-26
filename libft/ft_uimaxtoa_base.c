bbf9cae4d4bfa459092e0fe7297d27382252c2c7
Ignacio Velutini <ignaciovelutini@gmail.com>
2018-01-25 17:12:28 -0800

    eso

--- libft/ft_uimaxtoa_base.c
+++ libft/ft_uimaxtoa_base.c
@@ -6,7 +6,7 @@
 /*   By: ivelutin <ivelutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 22:09:16 by ivelutin          #+#    #+#             */
-/*   Updated: 2018/01/18 12:25:10 by ivelutin         ###   ########.fr       */
+/*   Updated: 2018/01/25 17:12:23 by ivelutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ char	*ft_uimaxtoa_base(uintmax_t value, int8_t base, const char *str)
 
 	i = value;
 	size = 1;
-	ret = 1;
 	while ((i /= base))
 		size++;
-	if (ret)
-	{
-		ret = (char *)malloc(sizeof(char) * (size + 1));
-		ret[size] = '\0';
-		i = value;
-		ret[--size] = str[i % base];
-	}
+	if (!(ret = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	ret[size] = '\0';
+	i = value;
+	ret[--size] = str[i % base];
 	while ((i /= base))
 		ret[--size] = str[i % base];
 	return (ret);
