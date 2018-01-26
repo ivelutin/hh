7ef6c6e56e42c65e943bdfe86ab5978add55b554
Ignacio Velutini <ignaciovelutini@gmail.com>
2018-01-25 17:14:22 -0800

    ahi

--- libft/ft_imaxtoa.c
+++ libft/ft_imaxtoa.c
@@ -6,7 +6,7 @@
 /*   By: ivelutin <ivelutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 22:08:42 by ivelutin          #+#    #+#             */
-/*   Updated: 2018/01/18 12:33:18 by ivelutin         ###   ########.fr       */
+/*   Updated: 2018/01/25 17:14:05 by ivelutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,15 @@ char	*ft_imaxtoa(intmax_t value)
 	intmax_t	size;
 	char		*ret;
 
-	ret = 1;
 	i = (value < 0) ? -value : value;
 	size = 1 + (value < 0);
 	while ((i /= 10))
 		size++;
-	if (ret)
-	{
-		ret = (char *)malloc(sizeof(char) * (size + 1));
-		ret[size] = '\0';
-		i = (value < 0) ? -value : value;
-		ret[--size] = "0123456789"[i % 10];
-	}
+	if (!(ret = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	ret[size] = '\0';
+	i = (value < 0) ? -value : value;
+	ret[--size] = "0123456789"[i % 10];
 	while ((i /= 10))
 		ret[--size] = "0123456789"[i % 10];
 	if (value < 0)
