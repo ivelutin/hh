81a7d4ffea7008132ebb170e881144a558ce4ac7
Ignacio Velutini <ignaciovelutini@gmail.com>
2018-01-25 17:19:04 -0800

    dd

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: ivelutin <ivelutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 22:09:09 by ivelutin          #+#    #+#             */
-/*   Updated: 2018/01/18 16:46:21 by ivelutin         ###   ########.fr       */
+/*   Updated: 2018/01/25 17:18:32 by ivelutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ char	*ft_strndup(const char *src, size_t len)
 	char	*cdst;
 
 	if (!(dst = ft_strnew(len + 1)))
-		return (0);
+		return NULL;
+	cdst = NULL;
 	if (dst)
 	{
 		cdst = dst;
